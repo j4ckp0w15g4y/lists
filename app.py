117536be97a53fa5bac62df8ff88a3d2a7362a4b
Jack Gay <jackpowis@gmail.com>
2019-08-30 16:18:37 +0100

    commit list basics

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 ##########################
 # Lists
 
-friends = ["Kevin", "Karen", "Jim"]
-#               0      1       2
+friends = ["Kevin", "Karen", "Jim", "Oscar", "Toby"]
+#
 print(friends[-3])
+print(friends[1:3])
 
- 
+
+friends[1] = 'Mike'
+
+print(friends[1])
